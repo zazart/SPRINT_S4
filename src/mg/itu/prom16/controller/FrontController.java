344197a681f3c498a224dc478e587ecbda737318
zazart <153142376+zazart@users.noreply.github.com>
2024-05-14 15:05:20 +0300

    init param non ok

--- src/mg/itu/prom16/controller/FrontController.java
+++ src/mg/itu/prom16/controller/FrontController.java
@@ -1,7 +1,17 @@
 package mg.itu.prom16.controller;
 
+import mg.itu.prom16.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.List;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.*;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -9,12 +19,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontController extends HttpServlet {
+    private String controllerPackage;
+    private List<String> controller = new ArrayList<>();
+    boolean checked = false;
+
+    @Override
+    public void init() throws ServletException {
+        controllerPackage = getServletConfig().getInitParameter("controller-package");
+        controllerPackage = "mg.test.controller";
+        super.init();
+    }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        out.println(request.getRequestURL());
+        if (!checked){
+            scan();
+            checked = true;
+        }
+        out.println("Vos controlleurs : \n");
+        for (String className : controller) {
+            out.println(className);
+        }
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
@@ -23,6 +50,34 @@ public class FrontController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
+    }
+
+
+    public void scan() {
+        try {
+            String classesPath = getServletContext().getRealPath("/WEB-INF/classes");
+            String decodedPath = URLDecoder.decode(classesPath, "UTF-8");
+            String packagePath = decodedPath +"\\"+ controllerPackage.replace('.', '\\');
+            File packageDirectory = new File(packagePath);
+            if (packageDirectory.exists() && packageDirectory.isDirectory()) {
+                File[] classFiles = packageDirectory.listFiles((dir, name) -> name.endsWith(".class"));
+                if (classFiles != null) {
+                    for (File classFile : classFiles) {
+                        String className = controllerPackage + '.' + classFile.getName().substring(0, classFile.getName().length() - 6);
+                        try {
+                            Class<?> classe = Class.forName(className);
+                            if (classe.isAnnotationPresent(Controller.class)) {
+                                controller.add(className);
+                            }
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
