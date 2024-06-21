e76cc2f518e767683939ec716011414e1ee4a940
zazart <153142376+zazart@users.noreply.github.com>
2024-06-21 21:01:46 +0300

    sprint 7 done

--- src/mg/itu/prom16/annotation/Objet.java
+++ src/mg/itu/prom16/annotation/Objet.java
@@ -5,10 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.PARAMETER)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Param {
-    String value();
+public @interface Objet {
+       
 }
 
 
